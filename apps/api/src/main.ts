f4cf9427e43f385f317cdb5c1e836726e3db3ff4
Rodrigo Ponce de Leon <rodrigoponcedeleon@Macmini.hitronhub.home>
2026-04-15 04:09:53 -0400

    fix: enable cors for wristcaviar frontend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://wristcaviar.fraterunion.com',
+    ],
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
